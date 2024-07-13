24570d8351768f9dfe90921acd819686c71d086f
OoIIIIIIIIoO <38577218+OoIIIIIIIIoO@users.noreply.github.com>
2024-07-13 00:38:34 -0400

    cluster

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ from cifar10 import run_cifar10
 # session. You can use this to either run a sequence of jobs locally
 # on your machine, or to run a sequence of jobs one after another
 # in an interactive shell on odyssey.
-DRYRUN = True
+DRYRUN = False
 
 # This is the base directory where the results will be stored.
 # On Odyssey, you may not want this to be your home directory
