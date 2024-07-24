7c51988d4c0865d682f0826dd1684fbbf24e378c
OoIIIIIIIIoO <38577218+OoIIIIIIIIoO@users.noreply.github.com>
2024-07-24 04:05:50 -0400

    cifar first experiment

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ import time
 # session. You can use this to either run a sequence of jobs locally
 # on your machine, or to run a sequence of jobs one after another
 # in an interactive shell on odyssey.
-DRYRUN = True
+DRYRUN = False
 
 # This is the base directory where the results will be stored.
 # On Odyssey, you may not want this to be your home directory
