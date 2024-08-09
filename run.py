ab4d58c1b8c53779225c286d7497d825bc32eeab
Theodora Boulouta <theodoraboulouta@gmail.com>
2024-08-09 10:37:10 -0400

    current results

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ DRYRUN = False
 # On Odyssey, you may not want this to be your home directory
 # If you're storing lots of files (or storing a lot of data).
 #OUTPUT_DIR = 'mnist_official'
-OUTPUT_DIR = 'mnist_official_gss'
+OUTPUT_DIR = 'mnist_official2'
 
 # This list contains the jobs and hyper-parameters to search over.
 # The list consists of tuples, in which the first element is
--- tmp/slurm_mnist.sh
+++ tmp/slurm_mnist.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 1-00:00 # Runtime in D-HH:MM
 #SBATCH -p shared 
 #SBATCH --mem=100GB # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o mnist_official_gss/mnist/out_%j.txt # File to which STDOUT will be written
-#SBATCH -e mnist_official_gss/mnist/err_%j.txt # File to which STDERR will be written
+#SBATCH -o mnist_official2/mnist/out_%j.txt # File to which STDOUT will be written
+#SBATCH -e mnist_official2/mnist/err_%j.txt # File to which STDERR will be written
 
 module load cuda/12.2.0-fasrc01
 module load python/3.10.9-fasrc01
@@ -17,4 +17,4 @@ VENV_PATH="/n/home12/thb489/new_continual_learning/continual-learning/myenv"
 cd ${MAIN_PATH}
 source "${VENV_PATH}/bin/activate"
 
-python -u ${MAIN_PATH}/run.py mnist_official_gss/mnist mnist '{"p": 0.9, "T": 5, "learning_rate": 0.001, "batch_size": 50, "num_centroids": 4, "model_training_epoch": 30, "early_stopping_threshold": 1000000, "random_seed": 4, "class_balanced": true, "execute_early_stopping": false}'
+python -u ${MAIN_PATH}/run.py mnist_official2/mnist mnist '{"p": 0.9, "T": 5, "learning_rate": 0.001, "batch_size": 50, "num_centroids": 4, "model_training_epoch": 30, "early_stopping_threshold": 1000000, "random_seed": 4, "class_balanced": true, "execute_early_stopping": false}'
