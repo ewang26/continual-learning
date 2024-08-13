b5c778ce893fa469daf47b579f6b8661c29790d0
OoIIIIIIIIoO <38577218+OoIIIIIIIIoO@users.noreply.github.com>
2024-08-13 17:12:39 -0400

    zero grad

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ DRYRUN = False
 # If you're storing lots of files (or storing a lot of data).
 # OUTPUT_DIR = 'cifar_test'
 #OUTPUT_DIR = 'official_cifar10'
-OUTPUT_DIR = 'erik_reproduce_results'
+OUTPUT_DIR = 'erik_reproduce_results_zero_grad'
 
 # This list contains the jobs and hyper-parameters to search over.
 # The list consists of tuples, in which the first element is
@@ -86,22 +86,22 @@ OUTPUT_DIR = 'erik_reproduce_results'
 # ]
 
 #TEST CIFAR
-# QUEUE = [
-#     ('cifar10', dict(
-#         p=[0.5], 
-#         T=[5],
-#         learning_rate=[0.5], # consider [0.01, 0.005, 0.001]
-#         batch_size=[10], # consider [10, 30, 50, 65]
-#         num_centroids=[4], 
-#         model_training_epoch=[1], # consider [10, 20, 50]
-#         early_stopping_threshold=[5.], # consider [0.1, 0.5, 1., 5., 10.]
-#         random_seed=[1],
-#         class_balanced=[True],
-#         max_data_size=[100], 
-#         execute_early_stopping=[False]
-#         ),
-#     ),
-# ]
+QUEUE = [
+    ('cifar10', dict(
+        p=[0.5], 
+        T=[5],
+        learning_rate=[0.5], # consider [0.01, 0.005, 0.001]
+        batch_size=[10], # consider [10, 30, 50, 65]
+        num_centroids=[4], 
+        model_training_epoch=[1], # consider [10, 20, 50]
+        early_stopping_threshold=[5.], # consider [0.1, 0.5, 1., 5., 10.]
+        random_seed=[1],
+        class_balanced=[True],
+        max_data_size=[100], 
+        execute_early_stopping=[False]
+        ),
+    ),
+]
 
 
 # # # #CIFAR FULL
