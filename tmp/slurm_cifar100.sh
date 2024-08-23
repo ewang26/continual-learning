2f8e5b884b2e2256e588ac0f6703516fb2e21b09
OoIIIIIIIIoO <38577218+OoIIIIIIIIoO@users.noreply.github.com>
2024-08-23 11:57:14 -0400

    cifar100 params

--- tmp/slurm_cifar100.sh
+++ tmp/slurm_cifar100.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -n 1 # Number of cores
 #SBATCH -N 1 # Ensure that all cores are on one machine
-#SBATCH -t 3-00:00 # Runtime in D-HH:MM
+#SBATCH -t 1-00:00 # Runtime in D-HH:MM
 #SBATCH -p gpu # GPU partition
 #SBATCH --gres=gpu:1 # Request 1 GPU
-#SBATCH --mem=1000GB # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=100GB # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o test_cifar100/cifar100/out_%j.txt # File to which STDOUT will be written
 #SBATCH -e test_cifar100/cifar100/err_%j.txt # File to which STDERR will be written
 
